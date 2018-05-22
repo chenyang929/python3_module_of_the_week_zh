63cd49f180a8aae9eff38e91a2af4114bf1f3d93
Chen Yang <chenyang929code@gmail.com>
2018-05-22 09:50:10 +0800

    add bisect

---
+++ Data Structures/bisect/bisect_example2.py
@@ -0,0 +1,14 @@
+# bisect_example2.py
+
+import bisect
+
+values = [14, 85, 77, 26, 50, 45, 66, 79, 10, 3, 84, 77, 1]
+
+print('New Pos Contents')
+print('--- --- --------')
+
+l = []
+for i in values:
+    position = bisect.bisect_left(l, i)
+    bisect.insort_left(l, i)
+    print('{:3} {:3}'.format(i, position), l)
